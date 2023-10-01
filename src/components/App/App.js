cb5c63be6860ef3f3beef6766435f1a79e89051e
Paul HoangLong <paulhoanglong@gmail.com>
2023-10-01 11:27:36 -0500

    Initial file setup and installation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,23 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
+import Details from './Details/Details';
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
+      
+      {/* Home Page */}
       <Router>        
         <Route path="/" exact>
           <MovieList />
         </Route>
         
-        {/* Details page */}
-
-        {/* Add Movie page */}
+        {/* Details page */}     
+        <Route path="/details">
+          <Details/>
+        </Route>
       </Router>
     </div>
   );
