066698aa3c991c7cc9d4717decac460bcc3f3e3d
Paul HoangLong <paulhoanglong@gmail.com>
2023-10-02 02:44:48 -0500

    Edit page done

--- src/components/Details/Edit.jsx
+++ src/components/Details/Edit.jsx
@@ -1,45 +1,87 @@
 import {
-    Box,
-    Button,
-    Checkbox,
-    Container,
-    FormControl,
-    FormControlLabel,
-    FormGroup,
-    FormLabel,
-    Grid,
-    TextField,
-    Typography,
-  } from "@mui/material";
-  
-  export default function Edit() {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 
-    return (
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
+export default function Edit() {
+  const dispatch = useDispatch();
+  const params = useParams();
+  const history = useHistory();
+
+  //Movie details
+  const description = useSelector((store) => store.description);
+  const title = useSelector((store) => store.title);
+
+  //On load, get movide details call to database
+  useEffect(() => {
+    dispatch({ type: "FETCH_DETAILS", payload: params.id });
+  }, []);
+
+ //Data to send
+ let [movieToUpdate, setMovieToUpdate] = useState({
+  title: "",
+  description: "",
+  id: params.id,
+});
+
+
+//Event listeners on inputs
+const handleTitleChange = (event) => {
+  setMovieToUpdate({ ...movieToUpdate, title: event.target.value });
+};
+const handleDescriptionChange = (event) => {
+  setMovieToUpdate({ ...movieToUpdate, description: event.target.value });
+};
+
+  //Submit form
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: "UPDATE_MOVIE",
+      payload: movieToUpdate
+    });
+    history.push(`/details/${params.id}/edit/success`)
+  };
+
+  //Cancel button
+  const handleBack = () => {
+    history.push(`/details/${params.id}`)
+  }
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
         <Container maxWidth="sm" sx={{ p: 3 }}>
-        <Typography sx={{ m: 2}} variant="h4" align="center" component="div">
-            Add a movie!
+          <Typography sx={{ m: 2 }} variant="h4" align="center" component="div">
+            Edit movie
           </Typography>
-          <Grid container spacing={2} sx={{border:1, borderRadius: '16px' }}>
+          <Grid container spacing={2} sx={{ border: 1, borderRadius: "16px" }}>
             <Grid container item xs={6} direction="column">
               <TextField
                 required
                 id="outlined-required"
                 label="Movie Title"
-                variant="outlined"
-              />
-              <TextField
-                required
-                id="outlined-required"
-                label="Movie Poster Link"
-                variant="outlined"
+                variant="filled"
+                defaultValue={title}
+                onChange={handleTitleChange}
               />
             </Grid>
             <Grid container item xs={6} direction="column">
@@ -49,93 +91,33 @@ import {
                 label="Movie Description"
                 multiline
                 rows={4}
-                variant="outlined"
+                variant="filled"
+                defaultValue={description}
+                onChange={handleDescriptionChange}
               />
             </Grid>
-            <Grid container item xs={6} direction="column">
-              <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
-                <FormLabel component="legend">Assign genres</FormLabel>
-                <FormGroup>
-                  <FormControlLabel
-                    control={<Checkbox name="Adventure" />}
-                    label="Adventure"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox name="Animated" />}
-                    label="Animated"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox name="Biographical" />}
-                    label="Biographical"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox name="Comedy" />}
-                    label="Comedy"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox name="Disaster" />}
-                    label="Disaster"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox name="Drama" />}
-                    label="Drama"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox name="Epic" />}
-                    label="Epic"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox name="Fantasy" />}
-                    label="Fantasy"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox name="Musical" />}
-                    label="Musical"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox name="Romantic" />}
-                    label="Romantic"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox name="Science Fiction" />}
-                    label="Science Fiction"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox name="Space-Opera" />}
-                    label="Space-Opera"
-                  />
-                   <FormControlLabel
-                    control={<Checkbox name="Superhero" />}
-                    label="Superhero"
-                  />
-                </FormGroup>
-              </FormControl>
-            </Grid>
-            <Grid container item xs={6} direction="column">
-              <Button
-                type="submit"
-                variant="outlined"
-                size="large"
-                color="success"
-                sx={{ m: 2 }}
-              >
-                Add
-              </Button>
-              <Button
-                type="submit"
-                variant="outlined"
-                size="large"
-                color="error"
-                sx={{ m: 2 }}
-              >
-                Cancel
-              </Button>
-            </Grid>
           </Grid>
-  
-     
+          <Button
+            type="submit"
+            variant="outlined"
+            size="large"
+            color="success"
+            sx={{ m: 2 }}
+            onClick={handleSubmit}
+          >
+            Update
+          </Button>
+          <Button
+            type="submit"
+            variant="outlined"
+            size="large"
+            color="error"
+            sx={{ m: 2 }}
+            onClick={handleBack}
+          >
+            Cancel
+          </Button>
         </Container>
-      </Box>
-    );
-  }
-  
+    </Box>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App.js';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App.js";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 // Provider allows us to use redux within our react app
-import { Provider } from 'react-redux';
-import logger from 'redux-logger';
+import { Provider } from "react-redux";
+import logger from "redux-logger";
 // Import saga middleware
-import createSagaMiddleware from 'redux-saga';
-import { takeEvery, put } from 'redux-saga/effects';
-import axios from 'axios';
+import createSagaMiddleware from "redux-saga";
+import { takeEvery, put } from "redux-saga/effects";
+import axios from "axios";
 
 //Fonts
 import "@fontsource/roboto/300.css";
@@ -19,75 +19,86 @@ import "@fontsource/roboto/700.css";
 
 // Create the rootSaga generator function
 function* rootSaga() {
-    yield takeEvery('FETCH_MOVIES', fetchAllMovies);
-    yield takeEvery('FETCH_DETAILS', getDescription);
-    yield takeEvery('FETCH_DETAILS', getTitle);
-    yield takeEvery('FETCH_DETAILS', getGenres);
-    yield takeEvery('FETCH_DETAILS', getPoster);
-    yield takeEvery('ADD_MOVIE', postMovie)
+  yield takeEvery("FETCH_MOVIES", fetchAllMovies);
+  yield takeEvery("FETCH_DETAILS", getDescription);
+  yield takeEvery("FETCH_DETAILS", getTitle);
+  yield takeEvery("FETCH_DETAILS", getGenres);
+  yield takeEvery("FETCH_DETAILS", getPoster);
+  yield takeEvery("ADD_MOVIE", postMovie);
+  yield takeEvery("UPDATE_MOVIE", updateMovie);
 }
 
 function* fetchAllMovies() {
-    // get all movies from the DB
-    try {
-        const movies = yield axios.get('/api/movie');
-        console.log('get all:', movies.data);
-        yield put({ type: 'SET_MOVIES', payload: movies.data });
-
-    } catch {
-        console.log('get all error');
-    }
-        
+  // get all movies from the DB
+  try {
+    const movies = yield axios.get("/api/movie");
+    console.log("get all:", movies.data);
+    yield put({ type: "SET_MOVIES", payload: movies.data });
+  } catch {
+    console.log("get all error");
+  }
 }
-
 
 //Get movie details based on id
 function* getDescription(action) {
-    try {
-      const details = yield axios.get(`/api/genre/${action.payload}`);
-      yield put({ type: 'SET_DESCRIPTION', payload: details.data[0].description});
-    } catch (error) {
-      console.log("error fetching images", error);
-    }
+  try {
+    const details = yield axios.get(`/api/genre/${action.payload}`);
+    yield put({
+      type: "SET_DESCRIPTION",
+      payload: details.data[0].description,
+    });
+  } catch (error) {
+    console.log("error getting description", error);
   }
+}
 
-  //Get movie title based on id
-  function* getTitle(action) {
-    try {
-      const details = yield axios.get(`/api/genre/${action.payload}`);
-      yield put({ type: 'SET_TITLE', payload: details.data[0].title});
-    } catch (error) {
-      console.log("error fetching images", error);
-    }
+//Get movie title based on id
+function* getTitle(action) {
+  try {
+    const details = yield axios.get(`/api/genre/${action.payload}`);
+    yield put({ type: "SET_TITLE", payload: details.data[0].title });
+  } catch (error) {
+    console.log("error getting title", error);
   }
+}
 
-  //Get movie genres based on id
-  function* getGenres(action) {
-    try {
-      const details = yield axios.get(`/api/genre/${action.payload}`);
-      yield put({ type: 'SET_GENRES', payload: details.data[0].genres});
-    } catch (error) {
-      console.log("error fetching images", error);
-    }
+//Get movie genres based on id
+function* getGenres(action) {
+  try {
+    const details = yield axios.get(`/api/genre/${action.payload}`);
+    yield put({ type: "SET_GENRES", payload: details.data[0].genres });
+  } catch (error) {
+    console.log("error getting genre", error);
   }
+}
 
-   //Get movie genres based on id
-   function* getPoster(action) {
-    try {
-      const details = yield axios.get(`/api/genre/${action.payload}`);
-      yield put({ type: 'SET_POSTER', payload: details.data[0].poster});
-    } catch (error) {
-      console.log("error fetching images", error);
-    }
+//Get movie genres based on id
+function* getPoster(action) {
+  try {
+    const details = yield axios.get(`/api/genre/${action.payload}`);
+    yield put({ type: "SET_POSTER", payload: details.data[0].poster });
+  } catch (error) {
+    console.log("error getting poster", error);
   }
+}
 
-  function* postMovie(action) {
-    try {
-        yield axios.post('/api/movie', action.payload);
-        yield put({ type: 'FETCH_MOVIES' });
-    } catch (error) {
-        console.log('error posting a movie', error);
-    }    
+//POST MOVIE
+function* postMovie(action) {
+  try {
+    yield axios.post("/api/movie", action.payload);
+    yield put({ type: "FETCH_MOVIES" });
+  } catch (error) {
+    console.log("error posting a movie", error);
+  }
+}
+
+//PUT MOVIE
+function* updateMovie(action) {
+  try {
+    yield axios.put(`/api/movie/${action.payload.id}`, action.payload);
+  } catch (error) {
+    console.log("error updating movie", error);
+  }
 }
 
 // Create sagaMiddleware
@@ -95,75 +106,75 @@ const sagaMiddleware = createSagaMiddleware();
 
 // Used to store movies returned from the server
 const movies = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_MOVIES':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "SET_MOVIES":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 // Used to store the movie description
 const description = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_DESCRIPTION':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "SET_DESCRIPTION":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 // Used to store the movie title
 const title = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_TITLE':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "SET_TITLE":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 // Used to store the movie genres
 const genres = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_GENRES':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "SET_GENRES":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 // Used to store the movie poster
 const poster = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_POSTER':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "SET_POSTER":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 // Create one store that all components can use
 const storeInstance = createStore(
-    combineReducers({
-        movies,
-        description,
-        title,
-        genres,
-        poster
-    }),
-    // Add sagaMiddleware to our store
-    applyMiddleware(sagaMiddleware, logger),
+  combineReducers({
+    movies,
+    description,
+    title,
+    genres,
+    poster,
+  }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger)
 );
 
 // Pass rootSaga into our sagaMiddleware
 sagaMiddleware.run(rootSaga);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Provider store={storeInstance}>
-            <App />
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={storeInstance}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
